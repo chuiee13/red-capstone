b13758e7e8b51ac36df2f46d09c0c81077a861b4
chuiee13 <yang.chungwon@gmail.com>
2016-12-07 17:06:22 -0800

    final project

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    $(document).ready(function(){
+      $('.top-logo').fadeIn(400);
+      $('.question.q1').delay(500).slideDown(200);
+      $('.question.q3').delay(700).slideDown(200);
+      $('.question.q2').delay(900).slideDown(200);
+      $('.social-media').delay(1100).fadeIn(500);
+      $('.submit-button').delay(1100).fadeIn(500);
+    });
+
+});
